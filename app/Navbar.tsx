b5d4ad5bcfb23b8ea176def97e4b6c99becbd74e
sujalshrestha/10 <suzolshrestha@gmail.com>
2025-11-14 22:30:57 +0545

    ok

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,9 +1,8 @@
 "use client";
 import Image from "next/image";
-
+import { useState } from "react";
 export default function Navbar() {
-
-
+  const [option, setOption] = useState(false);
   return (
     <>
       <div className=" flex items-center justify-between ">
@@ -17,9 +16,8 @@ export default function Navbar() {
         />
 
         <div
-          onClick={() => {
-          }}
-          className=" flex text-lg  rounded-md items-center  justify-between gap-3.5 h-10 px-4 bg-[hsl(243,27%,20%)]"
+          onClick={() => setOption((prev) => !prev)}
+          className=" flex text-lg  rounded-md items-center cursor-pointer justify-between gap-3.5 h-10 px-4 bg-[hsl(243,27%,20%)]"
         >
           <Image
             src="/images/icon-units.svg"
@@ -34,6 +32,13 @@ export default function Navbar() {
             height={15}
             width={15}
           />
+          {option && (
+            <div className="">
+              <p className="">Km/hr</p>
+              <p className="">Mp/s</p>
+              <p className="">Knots</p>
+            </div>
+          )}
         </div>
       </div>
     </>
